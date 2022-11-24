919876cc2315e656d00ce7e1ac335818738d5a58
udaykumar <sivakumar.udayv@gmail.com>
2022-11-24 20:50:26 +0530

    first commit

---
+++ src/test/java/com/infy/in/azure/demo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.in.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
