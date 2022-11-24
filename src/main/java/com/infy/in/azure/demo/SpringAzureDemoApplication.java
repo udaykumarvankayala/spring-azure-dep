ca1cd6856f5db8ef3968061163e5bda8bc6ee028
udaykumar <sivakumar.udayv@gmail.com>
2022-11-24 21:25:10 +0530

    third commit

--- src/main/java/com/infy/in/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/infy/in/azure/demo/SpringAzureDemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
-    @GetMapping("/message")
+    @GetMapping("/msg")
      public String message(){
          return "congrats uday good job super good super";
 
