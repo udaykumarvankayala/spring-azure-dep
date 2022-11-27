e3b56453a122bb400e9910b304ba8c4292fd22b7
udaykumar <sivakumar.udayv@gmail.com>
2022-11-27 15:08:39 +0530

    commit code

--- src/main/java/com/infy/in/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/infy/in/azure/demo/SpringAzureDemoApplication.java
@@ -5,14 +5,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
+
+    public List<Country> createCountryList()
+    {
+        Country indiaCountry=new Country(1, "India");
+        Country chinaCountry=new Country(4, "China");
+        Country nepalCountry=new Country(3, "Nepal");
+        Country bhutanCountry=new Country(2, "Bhutan");
+
+        List<Country> listOfCountries = new ArrayList<Country>();
+        listOfCountries.add(indiaCountry);
+        listOfCountries.add(chinaCountry);
+        listOfCountries.add(nepalCountry);
+        listOfCountries.add(bhutanCountry);
+        return listOfCountries;
+    }
     @GetMapping("/msg")
-     public String message(){
+
+    public List<Country> getCountries()
+    {
+        List<Country> listOfCountries = new ArrayList<Country>();
+        listOfCountries=createCountryList();
+        return listOfCountries;
+    }
+     /*public String message(){
          return "congrats uday good job super good super";
 
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAzureDemoApplication.class, args);
